edd07036bb600c0be903a5a389a51cb4c8648c4e
Martin <martin.soethe@uni-flensburg.de>
2019-08-25 20:20:33 +0200

    Refactor output
    Adopting the first draft for a results format.

--- compute.py
+++ compute.py
@@ -2,16 +2,20 @@
 """
 
 import os
+import time
 
 from oemof.solph import EnergySystem, Model
+from oemof.tabular.tools.postprocessing import supply_results
 
 # DONT REMOVE THIS LINE!
 from oemof.tabular import datapackage  # noqa
 from oemof.tabular.facades import TYPEMAP
 import oemof.tabular.tools.postprocessing as pp
 import oemof.outputlib as outputlib
+from oemof.network import Bus
 
 import numpy as np
+import pandas as pd
 
 import plotly.graph_objs as go
 import plotly.offline as offline
@@ -25,9 +29,11 @@ for pk in packages:
 
     # create a results directory
     results_path = os.path.join(
-        "results", pk, "output")
+        "results", pk)
     if not os.path.exists(results_path):
         os.makedirs(results_path)
+
+    build_time = time.time()
 
     # create energy system object
     es = EnergySystem.from_datapackage(
@@ -35,6 +41,8 @@ for pk in packages:
         attributemap={},
         typemap=TYPEMAP,
     )
+
+    build_time = time.time() - build_time
 
     # collect fix costs
     # https://github.com/oemof/oemof/pull/396
@@ -52,12 +60,36 @@ for pk in packages:
     # get the results from the the solved model
     m.results = m.results()
 
-    pp.write_results(m, results_path)
-
     # store model statistics
     meta_results[pk] = outputlib.processing.meta_results(m)
     meta_results[pk]['obj_add_fix_cost'] = meta_results[pk]['objective'] + fix_cost
 
+    # pp.write_results(m, results_path)
+
+    # POC write results in modex format
+    results = {
+        "timebuild_oemof.csv":
+            "Time required to build the model,Seconds,%s\n" % build_time,
+        "constraints_oemof.csv":
+            "Number of constraints,#,%s\n" % meta_results[pk]['problem']['Number of constraints'],
+        "variables_omeof.csv":
+            "Number of variables,#,%s\n" % meta_results[pk]['problem']['Number of variables'],
+        "objective_omeof.csv":
+            "Objective value,EURO,%s\n" % meta_results[pk]['obj_add_fix_cost']
+    }
+
+    buses = [b.label for b in m.es.nodes if isinstance(b, Bus)]
+
+    production = []
+    for b in buses:
+        production.append(supply_results(results=m.results, es=m.es, bus=[b]))
+
+    pd.concat(production, axis=1).\
+        to_csv(os.path.join(results_path, "production_oemof" + ".csv"))
+
+    for fn, content in results.items():
+        with open(os.path.join(results_path, fn), "w") as f:
+            f.write(content)
 
 # view meta results
 columns = [
