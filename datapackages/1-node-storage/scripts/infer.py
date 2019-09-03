f9e01b2cda7b3fb22b44261feeea26a4cf363780
Martin <martin.soethe@uni-flensburg.de>
2019-09-03 15:31:53 +0200

    Update 1-node-storage example
    In the sample 1 node datasets data for region "Mid" was replaced with
    data for "South". It is expected, that Storage component is now used
    in this setup. The tabular datapackages are updated accordingly.

--- datapackages/1-node-storage/scripts/infer.py
+++ datapackages/1-node-storage/scripts/infer.py
@@ -8,7 +8,7 @@ from oemof.tabular.datapackage import building
 building.infer_metadata(
     package_name='1-node-storage',
     foreign_keys={
-        'bus': ['volatile', 'dispatchable', 'storage', 'load'],
+        'bus': ['volatile', 'dispatchable', 'storage', 'load', 'excess'],
         'profile': ['load', 'volatile'],
         'from_to_bus': ['link'],
         'chp': [],
