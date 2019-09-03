ef94a4987dd5b983bb2c247742a099803329705c
Martin <martin.soethe@uni-flensburg.de>
2019-09-03 15:15:41 +0200

    Update 1-node example

--- datapackages/1-node/scripts/infer.py
+++ datapackages/1-node/scripts/infer.py
@@ -8,7 +8,7 @@ from oemof.tabular.datapackage import building
 building.infer_metadata(
     package_name='1-node',
     foreign_keys={
-        'bus': ['volatile', 'dispatchable', 'storage', 'load'],
+        'bus': ['volatile', 'dispatchable', 'storage', 'load', 'excess'],
         'profile': ['load', 'volatile'],
         'from_to_bus': ['link'],
         'chp': [],
